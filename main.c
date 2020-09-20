4d7d8109a182b1b8c10912e1233a460826a3b790
Leonardo González <leonardo.gonzalez3077@alumnos.udg.mx>
2020-09-20 15:48:56 -0500

    Capuramos la edad e imprimimos la edad mas uno

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 
 int main(){
     char nom[100];
+    unsigned int edad;
 
 
     printf("hola mundo\n");
     printf("ingresa tu nombre");
     fgets(nom, sizeof(nom), stdin);
     printf("hola %s",nom);
+    printf("Ingresa tu edad");
+    scanf("%u", &edad);
+    printf("vas a cumplir %u", edad+1); 
 
     return 0; 
 }
